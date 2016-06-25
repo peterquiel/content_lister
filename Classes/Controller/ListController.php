73eae1b864fded387ea76718cfe4bec191cacb4b
Peter Quiel <peter.quiel@wallmedien.de>
2016-06-26 00:46:10 +0200

    Search form working

--- Classes/Controller/ListController.php
+++ Classes/Controller/ListController.php
@@ -8,240 +8,264 @@ use \TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
  * @author Peter Quiel - peter.quiel@gmail.com
  */
 class Tx_ContentLister_Controller_ListController extends ActionController {
+    public $uploadDir = 'uploads/tx_contentlister/';
 
-  public $uploadDir = 'uploads/tx_contentlister/';
-
-  public function initializeAction(){
-    t3lib_utility_Debug::debug($this->settings);
-  }
-
-  public function listAction() {
-    $this->initView();
-    $this->view->assign('renderList', true);
-    if($this->settings['flexform']['show_search']){
-      $this->view->assign('renderSearch', true);
+    public function initializeAction(){
+        t3lib_utility_Debug::debug($this->settings);
     }
-    $this->view->assign('listEntries', $this->fetchEntryList());
-  }
-
-  public function categoryAction() {
-    $this->initView();
-    $this->view->assign('renderCategory', true);
-  }
-
-  public function detailAction() {
-    $this->initView();
-    $this->view->assign('renderDetail', true);
-    if($this->request->hasArgument('showUid')){
-      $pageRepository = t3lib_div::makeInstance('\TYPO3\CMS\Frontend\Page\PageRepository');
-      $this->view->assign('detailEntry', $pageRepository->checkRecord($this->getSelectedTable(), $this->request->getArgument('showUid')));
-    } else{
-      // todo redirect to list view
-    }
-  }
-
-  private function initView(){
-    $this->view->assign('settings', $this->settings);
-    $templateFile = $this->getTemplateFile();
-    if (!$templateFile) {
-      $templateFile = t3lib_extMgm::extPath('content_lister') . 'Resources/Private/Templates/List/Index.html';
-    }
-    $this->view->setTemplatePathAndFilename($templateFile);
-  }
-
-  protected function getTemplateFile(){
-    $flexFormTemplateFile = $this->settings['flexform']['template_file'];
-    if($flexFormTemplateFile){
-      return $this->uploadDir . $flexFormTemplateFile;
-    } else {
-      return $this->conf['templateFile'];
-    }
-    return null;
-  }
-
-  protected function getSearchConnection(){
-    return ($this->piVars['sconnect'] == 'or') ? 'or' : 'and';
-  }
-
-  /**
-   * Holt die Firmenliste aus der Datenbank und gibt ein mysql result zurück.
-   * @return mysql_result
-   */
-  protected function fetchEntryList(){
-    if($this->entryResult != null){
-      return $this->entryResult;
-    }
-    $db = $GLOBALS['TYPO3_DB'];
-    $catCriteria = $this->getCategoryCriteria();
-    $searchCriteria = $this->getSearchCriteria();
-    if($catCriteria !== null && $searchCriteria !== null ){
-      $statement = $catCriteria . ' and ' . $searchCriteria;
-    } else {
-      $statement = ($catCriteria !== null) ? $catCriteria : $searchCriteria;
-    }
-    $statement = ($statement == null) ? $this->getRestrictStatement() : $statement . ' and ' . $this->getRestrictStatement();
-    if($this->getShowPaginator()){
-      $resultSizeRes = $db->sql_fetch_row($db->exec_SELECTquery('count(uid)',$this->getSelectedTable(),$statement ));
-      $this->resultSize = $resultSizeRes[0];
-      $limit = ($this->getPage()-1) * $this->getPageSize() . ', ' . $this->getPageSize();
-      $result = $db->exec_SELECTquery('*',$this->getSelectedTable(),$statement,'', $this->getOrderBy(),  $limit);
-    } else {
-      $result = $db->exec_SELECTquery('*',$this->getSelectedTable(),$statement,'', $this->getOrderBy());
-      $this->resultSize = $db->sql_num_rows($result);
-    }
-    t3lib_utility_Debug::debug($this->getSelectedTable());
-    return ($db->sql_num_rows($result) > 0) ? $result : null;
-  }
-
-  protected function getSelectedTable(){
-    return $this->settings['flexform']['select_table'];
-  }
-
-  protected function getShowPaginator(){
-    return $this->settings['flexform']['showPaginator'];
-  }
-
-  protected function getPage(){
-    $page = 1;
-    if($this->request->hasArgument('page')) {
-      $page = intval($this->request->getArgument('page'));
-    }
-    return ($page > 1) ? $page : 1;
-  }
-
-  protected function getPageSize(){
-    $pageSize = 1;
-    if($this->request->hasArgument('pageSize')){
-      $pageSize = intval($this->request->getArgument('pageSize'));
-    }
-    $pageSize = ($pageSize) ? $pageSize : $this->settings['flexform']['pageSize'];
-    return ($pageSize > 5) ? $pageSize : 5;
-  }
-
-  /* holt die list der categorien und gibt sie als mysql result zurück */
-  protected function fetchCategoryList(){
+    
     /**
-     * Die Funktion fullQuoteStr schützt die Datenbank for SQLInjection
-     *
-     */
-    $statement = $this->getCategoryCriteria();
-    $statement = ($statement == null) ? $this->getRestrictStatement() : $statement . ' and ' . $this->getRestrictStatement();
-    $result = $GLOBALS['TYPO3_DB']->exec_SELECTquery('distinct ' . $this->getCategory(),$this->getSelectedTable(), $statement ,'',$this->getCategory() );
-    $GLOBALS['TYPO3_DB']->sql_num_rows($result);
-    if($GLOBALS['TYPO3_DB']->sql_num_rows($result)> 0){
-      return $result;
-    }
-    return null;
-  }
-
-  /* holt eine eintrag für die detailansicht und gibt den eintrag zurück  */
-  protected function fetchDetailEntry(){
-    $id = intval($this->request->getArgmuent('showUid'));
-    if($id){
-      return $this->pi_getRecord($this->getSelectedTable(),$id);
-    }
-    return null;
-  }
-
-  /* erzeugt das where statement für die listen- und kategorieansicht, wenn die listenansicht durch kategorieen eingeschränkt ist */
-  protected function getCategoryCriteria(){
-    $categoryArray = $this->getShowCategory();
-    if(sizeof($categoryArray) == 0) {
-      return null;
-    }
-    for($i=0; $i < sizeof($categoryArray); $i++){
-      $categoryArray[$i] =  $GLOBALS['TYPO3_DB']->fullQuoteStr($categoryArray[$i], $this->getSelectedTable());
-    }
-    if($this->getCategory() == null){
-      echo '<div style="color:red">ERROR: Category view is selected but no Category is choosen. Select a category in the category tab.</div>';
-    }
-    return ' ' . $this->getCategory() . ' in  (' . implode(',', $categoryArray) . ') ';
-  }
-
-  /* erzeugt das where statement für die suche */
-  protected function getSearchCriteria(){
-    $searchWords = $this->getSearchWordsArray();
-    $searchFields = $this->getSearchIn();
-    $statementArray = array();
-    if(sizeof($searchWords ) > 0 && sizeof($searchFields)){
-      for($i=0; $i < sizeof($searchWords); $i++){
-        $wordStatemantArray = array();
-        for($j=0; $j< sizeof($searchFields); $j++){
-          $wordStatemantArray[$j] = 'LOWER('.$searchFields[$j]. ') like \'%' . strtolower($searchWords[$i]) . '%\'';
+     *  @param string $searchword
+     *  @param string $searchoperator
+     *   */
+    public function searchAction($searchword='*', $searchoperator='and') {
+        t3lib_utility_Debug::debug("searchword:". $searchword . " op:" . $searchoperator);
+        $this->initView();
+        $this->initSearch();
+        $this->view->assign('searchword', $searchword);
+        $this->view->assign('searchoperator', $searchoperator);
+        $this->view->assign('renderList', true);
+        if($this->settings['flexform']['show_search']){
+            $this->view->assign('renderSearch', true);
         }
-        $statementArray[$i] = ' ('.implode( ' or ',$wordStatemantArray).') ';
-      }
+        $this->view->assign('listEntries', $this->fetchEntryList());
     }
-    if(sizeof($statementArray)> 0){
-      return ' ('.implode( ' '.$this->getSearchConnection().' ',$statementArray).')';
-    } else {
-      return null;
+
+    public function listAction() {
+        $this->initView();
+        $this->initSearch();
+        t3lib_utility_Debug::debug($this->request->getArguments());
+        $this->view->assign('url', $this->uriBuilder->getRequest()->getRequestUri());
+
+        $this->view->assign('renderList', true);
+        $this->view->assign('listEntries', $this->fetchEntryList());
     }
-  }
 
-  protected function getSearchWordsArray(){
-    $words = array();
-    if($this->getSearchWords() ){
-      $words = explode (' ', $this->getSearchWords());
+    public function categoryAction() {
+        $this->initView();
+        $this->view->assign('renderCategory', true);
     }
-    return $words;
-  }
 
-  protected function getSearchWords(){
-    if($this->request->hasArgument('swords')){
-      return $this->request->getArgmuent('swords');
+    public function detailAction() {
+        $this->initView();
+        $this->view->assign('renderDetail', true);
+        if($this->request->hasArgument('showUid')){
+            $pageRepository = t3lib_div::makeInstance('\TYPO3\CMS\Frontend\Page\PageRepository');
+            $this->view->assign('detailEntry', $pageRepository->checkRecord($this->getSelectedTable(), $this->request->getArgument('showUid')));
+        } else{
+            // todo redirect to list view
+        }
     }
-    return null;
-  }
 
-  protected function getCategory(){
-    return $this->settings['flexform']['category'];
-  }
-
-
-  protected function getShowCategory(){
-    $formCategory = $this->settings['flexform']['show_category'];
-    $categoryArray = array();
-    if($this->request->hasArgument('category') || $formCategory){
-      if($formCategory){
-        $categoryArray = explode(',', $formCategory);
-      }
-      if($this->request->hasArgument('category')){
-        $categoryArray[] = trim($this->request->getArgument('category'));
-      }
+    private function initSearch() {
+        if($this->settings['flexform']['show_search']){
+            $this->view->assign('renderSearch', true);
+            $this->view->assign('searchoperatordata', array('or' => 'Oder', 'and' => 'Und'));
+        }
     }
-    return $categoryArray;
-  }
+    private function initView(){
+        $this->view->assign('settings', $this->settings);
 
-
-  protected function getOrderBy(){
-    $order = $this->settings['flexform']['order_by'];
-    if($order) {
-      $order .= ' ' .$this->settings['flexform']['order_by_asc'];
+        $templateFile = $this->getTemplateFile();
+        if (!$templateFile) {
+            $templateFile = t3lib_extMgm::extPath('content_lister') . 'Resources/Private/Templates/List/Index.html';
+        }
+        $this->view->setTemplatePathAndFilename($templateFile);
     }
-    return $order;
-  }
 
-  protected function getRestrictStatement(){
-    $where = ' deleted=0 and hidden=0 ';
-    $pids = $this->settings['flexform']['pages'];
-    $recursive =  $this->settings['flexform']['recursive'];
-    if($pids){
-      $pi = $this->objectManager->create('TYPO3\CMS\Frontend\Plugin\AbstractPlugin');
-      $pi->cObj = $this->configurationManager->getContentObject();
-      $pidList = $pi->pi_getPidList($pids, $recursive);
-      $where .= " and pid IN (".$pidList.") ";
+    protected function getTemplateFile(){
+        $flexFormTemplateFile = $this->settings['flexform']['template_file'];
+        if($flexFormTemplateFile){
+            return $this->uploadDir . $flexFormTemplateFile;
+        } else {
+            return $this->conf['templateFile'];
+        }
+        return null;
     }
-    return $where;
-  }
 
-  protected function getSearchIn(){
-    $searchin = $this->settings['flexform']['search_in'];
-    $searchFields = array();
-    if($searchin){
-      $searchFields = explode(',', $searchin);
+    protected function getSearchConnection(){
+        return ($this->piVars['sconnect'] == 'or') ? 'or' : 'and';
     }
-    return $searchFields;
-  }
+
+    /**
+     * Holt die Firmenliste aus der Datenbank und gibt ein mysql result zurück.
+     * @return mysql_result
+     */
+    protected function fetchEntryList(){
+        if($this->entryResult != null){
+            return $this->entryResult;
+        }
+        $db = $GLOBALS['TYPO3_DB'];
+        $catCriteria = $this->getCategoryCriteria();
+        $searchCriteria = $this->getSearchCriteria();
+        if($catCriteria !== null && $searchCriteria !== null ){
+            $statement = $catCriteria . ' and ' . $searchCriteria;
+        } else {
+            $statement = ($catCriteria !== null) ? $catCriteria : $searchCriteria;
+        }
+        $statement = ($statement == null) ? $this->getRestrictStatement() : $statement . ' and ' . $this->getRestrictStatement();
+        if($this->getShowPaginator()){
+            $resultSizeRes = $db->sql_fetch_row($db->exec_SELECTquery('count(uid)',$this->getSelectedTable(),$statement ));
+            $this->resultSize = $resultSizeRes[0];
+            $limit = ($this->getPage()-1) * $this->getPageSize() . ', ' . $this->getPageSize();
+            $result = $db->exec_SELECTquery('*',$this->getSelectedTable(),$statement,'', $this->getOrderBy(),  $limit);
+        } else {
+            $result = $db->exec_SELECTquery('*',$this->getSelectedTable(),$statement,'', $this->getOrderBy());
+            $this->resultSize = $db->sql_num_rows($result);
+        }
+        t3lib_utility_Debug::debug($this->getSelectedTable());
+        return ($db->sql_num_rows($result) > 0) ? $result : null;
+    }
+
+    protected function getSelectedTable(){
+        return $this->settings['flexform']['select_table'];
+    }
+
+    protected function getShowPaginator(){
+        return $this->settings['flexform']['showPaginator'];
+    }
+
+    protected function getPage(){
+        $page = 1;
+        if($this->request->hasArgument('page')) {
+            $page = intval($this->request->getArgument('page'));
+        }
+        return ($page > 1) ? $page : 1;
+    }
+
+    protected function getPageSize(){
+        $pageSize = 1;
+        if($this->request->hasArgument('pageSize')){
+            $pageSize = intval($this->request->getArgument('pageSize'));
+        }
+        $pageSize = ($pageSize) ? $pageSize : $this->settings['flexform']['pageSize'];
+        return ($pageSize > 5) ? $pageSize : 5;
+    }
+
+    /* holt die list der categorien und gibt sie als mysql result zurück */
+    protected function fetchCategoryList(){
+        /**
+         * Die Funktion fullQuoteStr schützt die Datenbank for SQLInjection
+         *
+         */
+        $statement = $this->getCategoryCriteria();
+        $statement = ($statement == null) ? $this->getRestrictStatement() : $statement . ' and ' . $this->getRestrictStatement();
+        $result = $GLOBALS['TYPO3_DB']->exec_SELECTquery('distinct ' . $this->getCategory(),$this->getSelectedTable(), $statement ,'',$this->getCategory() );
+        $GLOBALS['TYPO3_DB']->sql_num_rows($result);
+        if($GLOBALS['TYPO3_DB']->sql_num_rows($result)> 0){
+            return $result;
+        }
+        return null;
+    }
+
+    /* holt eine eintrag für die detailansicht und gibt den eintrag zurück  */
+    protected function fetchDetailEntry(){
+        $id = intval($this->request->getArgmuent('showUid'));
+        if($id){
+            return $this->pi_getRecord($this->getSelectedTable(),$id);
+        }
+        return null;
+    }
+
+    /* erzeugt das where statement für die listen- und kategorieansicht, wenn die listenansicht durch kategorieen eingeschränkt ist */
+    protected function getCategoryCriteria(){
+        $categoryArray = $this->getShowCategory();
+        if(sizeof($categoryArray) == 0) {
+            return null;
+        }
+        for($i=0; $i < sizeof($categoryArray); $i++){
+            $categoryArray[$i] =  $GLOBALS['TYPO3_DB']->fullQuoteStr($categoryArray[$i], $this->getSelectedTable());
+        }
+        if($this->getCategory() == null){
+            echo '<div style="color:red">ERROR: Category view is selected but no Category is choosen. Select a category in the category tab.</div>';
+        }
+        return ' ' . $this->getCategory() . ' in  (' . implode(',', $categoryArray) . ') ';
+    }
+
+    /* erzeugt das where statement für die suche */
+    protected function getSearchCriteria(){
+        $searchWords = $this->getSearchWordsArray();
+        $searchFields = $this->getSearchIn();
+        $statementArray = array();
+        if(sizeof($searchWords ) > 0 && sizeof($searchFields)){
+            for($i=0; $i < sizeof($searchWords); $i++){
+                $wordStatemantArray = array();
+                for($j=0; $j< sizeof($searchFields); $j++){
+                    $wordStatemantArray[$j] = 'LOWER('.$searchFields[$j]. ') like \'%' . strtolower($searchWords[$i]) . '%\'';
+                }
+                $statementArray[$i] = ' ('.implode( ' or ',$wordStatemantArray).') ';
+            }
+        }
+        if(sizeof($statementArray)> 0){
+            return ' ('.implode( ' '.$this->getSearchConnection().' ',$statementArray).')';
+        } else {
+            return null;
+        }
+    }
+
+    protected function getSearchWordsArray(){
+        $words = array();
+        if($this->getSearchWords() ){
+            $words = explode (' ', $this->getSearchWords());
+        }
+        return $words;
+    }
+
+    protected function getSearchWords(){
+        if($this->request->hasArgument('swords')){
+            return $this->request->getArgmuent('swords');
+        }
+        return null;
+    }
+
+    protected function getCategory(){
+        return $this->settings['flexform']['category'];
+    }
+
+
+    protected function getShowCategory(){
+        $formCategory = $this->settings['flexform']['show_category'];
+        $categoryArray = array();
+        if($this->request->hasArgument('category') || $formCategory){
+            if($formCategory){
+                $categoryArray = explode(',', $formCategory);
+            }
+            if($this->request->hasArgument('category')){
+                $categoryArray[] = trim($this->request->getArgument('category'));
+            }
+        }
+        return $categoryArray;
+    }
+
+
+    protected function getOrderBy(){
+        $order = $this->settings['flexform']['order_by'];
+        if($order) {
+            $order .= ' ' .$this->settings['flexform']['order_by_asc'];
+        }
+        return $order;
+    }
+
+    protected function getRestrictStatement(){
+        $where = ' deleted=0 and hidden=0 ';
+        $pids = $this->settings['flexform']['pages'];
+        $recursive =  $this->settings['flexform']['recursive'];
+        if($pids){
+            $pi = $this->objectManager->create('TYPO3\CMS\Frontend\Plugin\AbstractPlugin');
+            $pi->cObj = $this->configurationManager->getContentObject();
+            $pidList = $pi->pi_getPidList($pids, $recursive);
+            $where .= " and pid IN (".$pidList.") ";
+        }
+        return $where;
+    }
+
+    protected function getSearchIn(){
+        $searchin = $this->settings['flexform']['search_in'];
+        $searchFields = array();
+        if($searchin){
+            $searchFields = explode(',', $searchin);
+        }
+        return $searchFields;
+    }
 }
 ?>
--- Classes/ItemsProcFunction.php
+++ Classes/ItemsProcFunction.php
@@ -1,107 +1,110 @@
 <?php
 
-use \TYPO3\CMS\Core\Utility\GeneralUtility; 
+use \TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class Tx_ContentLister_ItemsProcFunction {
 
-  protected function getCurrentId(){
-    return substr(GeneralUtility::_GP('returnUrl'), strrpos(GeneralUtility::_GP('returnUrl') , '=') +1);
-  }
-
-  protected function getTablesFromTs(){
-    $ts = t3lib_BEfunc::getPagesTSconfig($this->getCurrentId());
-    if(is_array($ts)) {
-      return GeneralUtility::trimExplode(',', $ts['plugin.']['contentlister.']['tables']);
+    protected function getCurrentId(){
+        return substr(GeneralUtility::_GP('returnUrl'), strrpos(GeneralUtility::_GP('returnUrl') , '=') +1);
     }
-    return null;
-  }
 
-  protected function getExcludeColumns($config){
-    $ts = t3lib_BEfunc::getPagesTSconfig($this->getCurrentId());
-    if(is_array($ts)) {
-      $exclude = GeneralUtility::trimExplode(',', $ts['plugin.']['contentlister.']['exclude.'][$this->getSelectedTable($config)]);
-      if(is_array($exclude)){
-        foreach($exclude as $key => $value){
-          $exclude[$value] = 1;
+    protected function getTablesFromTs(){
+        $ts = t3lib_BEfunc::getPagesTSconfig($this->getCurrentId());
+        if(is_array($ts)) {
+            return GeneralUtility::trimExplode(',', $ts['plugin.']['contentlister.']['tables']);
         }
-        return $exclude;
-      }
+        return null;
     }
-    return array();
-  }
 
-  public function getSelectedTable($config){
-    $flexFormArray = GeneralUtility::xml2array($config['row']['pi_flexform']);
-    if(! is_array($flexFormArray)){
-      return null;
-    }
-    $table = $flexFormArray['data']['sDEF']['lDEF']['select_table']['vDEF'];
-    if($table){
-      GeneralUtility::loadTCA($table);
-      return $table;
-    }
-    return null;
-  }
-
-  public function getTables($config){
-    $tables= $this->getTablesFromTs();
-    if($tables){
-      $i=0;
-      foreach($tables as $table){
-        $config['items'][$i] = array($GLOBALS['LANG']->sL( $GLOBALS['TCA'][$table]['ctrl']['title'] ),$table);
-        $i++;
-      }
-    }
-    return $config;
-  }
-
-
-  public function getTableColumns($config) {
-    $this->getHelpTableCol($config);
-  }
-
-  public function getSearchColumns($config){
-    $this->getHelpTableCol($config);
-  }
-
-  public function getCategoryColumns($config){
-    $config['items'][0] = array('','');
-    return $this->getHelpTableCol($config, 1);
-  }
-
-  protected function getHelpTableCol($config, $startIndex = 0){
-    $table = $this->getSelectedTable($config);
-    $columns = $GLOBALS['TCA'][$table]['columns'];
-    if(! is_array($columns)){
-      return;
-    }
-    $i=$startIndex;
-    $exclude = $this->getExcludeColumns($config);
-    foreach($columns as $key => $value){
-      if($exclude[$key]){
-        continue;
-      }
-      $config['items'][$i] = array($GLOBALS['LANG']->sL($value['label']), $key);
-      $i ++;
-    }
-    return $config;	
-  }
-
-  public function getCategories($config){
-    $flexFormArray = GeneralUtility::xml2array($config['row']['pi_flexform']);
-    if(! is_array($flexFormArray)){
-      return;
-    }
-    $category = $flexFormArray['data']['Category']['lDEF']['category']['vDEF'];
-    $config['items'] = array();
-    if($category !== null && $category !== ''){
-      $result = $GLOBALS['TYPO3_DB']->exec_SELECTquery( 'distinct ' . $category, $this->getSelectedTable($config), '' ,'',$category );
-      if($result != null && $GLOBALS['TYPO3_DB']->sql_num_rows($result) > 0){
-        for($i=0; $entry = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($result); $i++){
-          $config['items'][$i] = array( $entry[$category], $entry[$category] );
+    protected function getExcludeColumns($config){
+        $ts = t3lib_BEfunc::getPagesTSconfig($this->getCurrentId());
+        if(is_array($ts)) {
+            $exclude = GeneralUtility::trimExplode(',', $ts['plugin.']['contentlister.']['exclude.'][$this->getSelectedTable($config)]);
+            if(is_array($exclude)){
+                foreach($exclude as $key => $value){
+                    $exclude[$value] = 1;
+                }
+                return $exclude;
+            }
         }
-      }
+        return array();
     }
-    return $config;
-  }
+
+    public function getSelectedTable($config){
+        //Tx_Extbase_Utility_Debugger::var_dump($config);
+        $flexFormArray = GeneralUtility::xml2array($config['row']['pi_flexform']);
+        //t3lib_utility_Debug::debug($flexFormArray, "debug flexFormArray:" );
+        if(! is_array($flexFormArray)){
+            return null;
+        }
+
+        $table = $flexFormArray['data']['sDEF']['lDEF']['settings.flexform.select_table']['vDEF'];
+        if($table){
+            GeneralUtility::loadTCA($table);
+            return $table;
+        }
+        return null;
+    }
+
+    public function getTables($config){
+        $tables= $this->getTablesFromTs();
+        if($tables){
+            $i=0;
+            foreach($tables as $table){
+                $config['items'][$i] = array($GLOBALS['LANG']->sL( $GLOBALS['TCA'][$table]['ctrl']['title'] ),$table);
+                $i++;
+            }
+        }
+        return $config;
+    }
+
+
+    public function getTableColumns($config) {
+        $this->getHelpTableCol($config);
+    }
+
+    public function getSearchColumns($config){
+        $this->getHelpTableCol($config);
+    }
+
+    public function getCategoryColumns($config){
+        $config['items'][0] = array('','');
+        return $this->getHelpTableCol($config, 1);
+    }
+
+    protected function getHelpTableCol($config, $startIndex = 0){
+        $table = $this->getSelectedTable($config);
+        $columns = $GLOBALS['TCA'][$table]['columns'];
+        if(! is_array($columns)){
+            return;
+        }
+        $i=$startIndex;
+        $exclude = $this->getExcludeColumns($config);
+        foreach($columns as $key => $value){
+            if($exclude[$key]){
+                continue;
+            }
+            $config['items'][$i] = array($GLOBALS['LANG']->sL($value['label']), $key);
+            $i ++;
+        }
+        return $config;
+    }
+
+    public function getCategories($config){
+        $flexFormArray = GeneralUtility::xml2array($config['row']['pi_flexform']);
+        if(! is_array($flexFormArray)){
+            return;
+        }
+        $category = $flexFormArray['data']['Category']['lDEF']['settings.flexform.category']['vDEF'];
+        $config['items'] = array();
+        if($category !== null && $category !== ''){
+            $result = $GLOBALS['TYPO3_DB']->exec_SELECTquery( 'distinct ' . $category, $this->getSelectedTable($config), '' ,'',$category );
+            if($result != null && $GLOBALS['TYPO3_DB']->sql_num_rows($result) > 0){
+                for($i=0; $entry = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($result); $i++){
+                    $config['items'][$i] = array( $entry[$category], $entry[$category] );
+                }
+            }
+        }
+        return $config;
+    }
 }
